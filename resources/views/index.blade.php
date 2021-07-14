542a372784ae564e22ee57ad7103b6e84fb8608d
Monalishacoding <monalishasahoo.official@gmail.com>
2021-07-14 23:02:28 +0530

    added featured services section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,4 +5,37 @@
 @section('body')
     {{-- Slider --}}
     @include('layouts.slider')
+
+    {{-- Featured Services --}}
+    <section id="ftServices" class="my-5 py-3">
+        <div class="container">
+            <div class="boxes-container">
+                <div class="box">
+                    <a href="#" class="img-box">
+                        <img src="/assets/images/flower.png" alt="Flowers">
+                    </a>
+                    <h3 class="my-3"> <a href="#"> Flowers</a></h3>
+                </div>
+                <div class="box">
+                    <a href="#" class="img-box">
+                        <img src="/assets/images/cakes.png" alt="Cakes">
+                    </a>
+                    <h3 class="my-3"><a href="#">Cakes</a></h3>
+                </div>
+                <div class="box">
+                    <a href="#" class="img-box">
+                        <img src="/assets/images/events.png" alt="Events">
+                    </a>
+                    <h3 class="my-3"><a href="#">Events</a></h3>
+                </div>
+                <div class="box">
+                    <a href="#" class="img-box">
+                        <img src="/assets/images/combo.png" alt="Combo">
+                    </a>
+                    <h3 class="my-3"><a href="#">Combo</a></h3>
+                </div>
+            </div>
+        </div>
+    </section>
+
 @endsection
