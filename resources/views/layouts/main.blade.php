711f24742c5313de956af41eb1b022c831ff9ed8
Monalishacoding <monalishasahoo.official@gmail.com>
2021-07-27 21:06:45 +0530

    cahanged in stylesheet

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,12 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title' , env('APP_NAME'))</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    {{-- <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;700&family=Josefin+Sans:wght@300;400;500&display=swap"
-        rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;700&family=Josefin+Sans:wght@300;400;500&display=swap" rel="stylesheet"> --}}
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"> 
 
     <link rel="stylesheet" href="/assets/css/style-v1.37.css">
     <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon">
