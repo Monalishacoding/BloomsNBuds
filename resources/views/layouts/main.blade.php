4962875a17e8f0d5216ff6534f862605d526f6e5
Monalishacoding <monalishasahoo.official@gmail.com>
2021-07-30 21:37:45 +0530

    service space created

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,9 +9,9 @@
     {{-- <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;700&family=Josefin+Sans:wght@300;400;500&display=swap" rel="stylesheet"> --}}
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"> 
 
     <link rel="stylesheet" href="/assets/css/style-v1.38.css">
--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -3,7 +3,76 @@
 @section('title') Our Services @endsection
 
 @section('body')
+<div class="container my-3">
+    <nav>
+        <ol class="breadcrumb-list">
+            <li class="breadcrumb-item"><a href="{{route('page.index')}}">Home</a></li>
+            <li class="breadcrumb-item active">Our Services</li>
+        </ol>
+    </nav>
+</div>
 
-Services
+{{-- Services --}}
+<section id="services-box" class="my-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 col-md-2 mb-4">
+                <div class="card">
+                    <img src="/assets/images/flower.png" alt="Flower">
+                    <div class="card-body">
+                        <h6 class="card-title"><a href="#" class="nav-link stretched-link">Exotic Flowers</a></h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-md-2 mb-4">
+                <div class="card">
+                    <img src="/assets/images/flower.png" alt="Flower">
+                    <div class="card-body">
+                        <h6 class="card-title"><a href="#" class="nav-link stretched-link">Hand Bouquet</a></h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-md-2 mb-4">
+                <div class="card">
+                    <img src="/assets/images/flower.png" alt="Flower">
+                    <div class="card-body">
+                        <h6 class="card-title"><a href="#" class="nav-link stretched-link">Podium Arrangements</a></h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-md-2 mb-4">
+                <div class="card">
+                    <img src="/assets/images/flower.png" alt="Flower">
+                    <div class="card-body">
+                        <h6 class="card-title"><a href="#" class="nav-link stretched-link">Customized Cakes</a></h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-md-2 mb-4">
+                <div class="card">
+                    <img src="/assets/images/flower.png" alt="Flower">
+                    <div class="card-body">
+                        <h6 class="card-title"><a href="#" class="nav-link stretched-link">Gifts</a></h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-6 col-md-2 mb-4">
+                <div class="card">
+                    <img src="/assets/images/flower.png" alt="Flower">
+                    <div class="card-body">
+                        <h6 class="card-title"><a href="#" class="nav-link stretched-link">Event</a></h6>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
 
 @endsection
